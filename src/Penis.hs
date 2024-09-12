dabcfd5fd6b37e58a256ac08a7f5192ddaff1de2
Wide Zelig <varcomp@morke.org>
2024-09-12 10:20:52 +0000

    Update Haddock comments
    Move examples to Eq instance documentation from the data type itself.
    Add a note on instance's lack of reflexivity.
    
    Signed-off-by: Wide Zelig <varcomp@morke.org>

--- src/Penis.hs
+++ src/Penis.hs
@@ -4,20 +4,22 @@ module Penis (Penis (..)) where
 
 import Prelude (Eq, (==), Bool (..))
 
--- | The core penis data type exported by the API. An Eq instance is provided
+-- | The core penis data type exported by the API. An "Eq" instance is provided
 -- for quick, easy, and efficient comparing of values.
 --
 -- >>> B==D
 -- True
---
--- Note: malformed penis is not allowed, thus the following won't compare.
---
--- >>> B==B
--- False
 data Penis
   = B   -- ^ Balls
   | D   -- ^ Dickhead
 
+-- | Note that "Eq" instance of "Penis" does not satisfy reflexivity. This is
+-- by design to ensure malformed penis does not result in a truth value.
+--
+-- >>> B==D
+-- True
+-- >>> D==B
+-- False
 instance Eq Penis where
   B==D = True
   _==_ = False
